6cb8ac983e8dcddac21b4177044dd5151150de8b
KarolisGib <karolisgibavicius@yahoo.com>
2024-05-04 21:49:08 +0300

    Implementation of Weapon

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
@@ -8,15 +9,39 @@ public class Main {
         Hero hero = new Hero();
         hero.name = "Hero";
         hero.health = 100;
-        hero.damage = rnd.nextInt(5) + 3;
+        hero.damage = 1;
         hero.defence = (rnd.nextDouble(51) / 2) / 100;
 
         Enemy enemy = new Enemy();
         enemy.name = "Enemy";
         enemy.health = 100;
-        enemy.damage = rnd.nextInt(5) + 3;
+        enemy.damage = 1;
         enemy.defence = (rnd.nextDouble(51) / 2) / 100;
 
-        Methods.fight(hero, enemy);
+        Sword rustySword = new Sword();
+        rustySword.name = "Sword";
+        rustySword.damage = 5;
+
+        Sword sword = new Sword();
+        sword.name = "Sword";
+        sword.damage = 8;
+
+        Sword enhancedSword = new Sword();
+        enhancedSword.name = "Sword";
+        enhancedSword.damage = 15;
+
+        ArrayList<Integer> heroChoice = new ArrayList<>();
+        heroChoice.add(hero.damage);
+        heroChoice.add(rustySword.damage);
+        heroChoice.add(sword.damage);
+        heroChoice.add(enhancedSword.damage);
+
+        ArrayList<Integer> enemyChoice = new ArrayList<>();
+        enemyChoice.add(enemy.damage);
+        enemyChoice.add(rustySword.damage);
+        enemyChoice.add(sword.damage);
+        enemyChoice.add(enhancedSword.damage);
+
+        Methods.fight(hero, enemy,  heroChoice, enemyChoice);
     }
 }
--- src/Methods.java
+++ src/Methods.java
@@ -1,16 +1,34 @@
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Methods {
 
     private static final DecimalFormat df = new DecimalFormat("0.0");
     static int counter = 0;
+    static Random random = new Random();
 
     static void info(Hero hero, Enemy enemy) {
         System.out.println(hero.name + " health is: " + hero.health);
-        System.out.println(hero.name + " damage is: " + hero.damage);
         System.out.println(enemy.name + " health is: " + enemy.health);
-        System.out.println(enemy.name + " damage is: " + enemy.damage);
         System.out.println();
+    }
+
+    static void fight(Hero hero, Enemy enemy, ArrayList<Integer> heroChoice, ArrayList<Integer> enemyChoice) {
+        info(hero, enemy);
+        int heroDamage = heroChoice.get(random.nextInt(heroChoice.size()));
+        int enemyDamage = enemyChoice.get(random.nextInt(enemyChoice.size()));
+        while (hero.health > 0 && enemy.health > 0) {
+            counter++;
+            System.out.println(heroDamage);
+            System.out.println(enemyDamage);
+            double heroHealth = (hero.health -= (enemyDamage - (enemyDamage * hero.defence)));
+            double enemyHealth = (enemy.health -= (heroDamage - (heroDamage * enemy.defence)));
+            System.out.println(hero.name + " health is: " + df.format(heroHealth));
+            System.out.println(enemy.name + " health is: " + df.format(enemyHealth));
+            System.out.println();
+        }
+        result(hero, enemy);
     }
 
     static void result(Hero hero, Enemy enemy) {
@@ -21,18 +39,5 @@ public class Methods {
         } else {
             System.out.println("Battle took " + counter + " rounds and " + enemy.name + " won");
         }
-    }
-
-    static void fight(Hero hero, Enemy enemy) {
-        info(hero, enemy);
-        while (hero.health > 0 && enemy.health > 0) {
-            counter++;
-            double heroHealth = (hero.health -= (enemy.damage - (enemy.damage * hero.defence)));
-            double enemyHealth = (enemy.health -= (hero.damage - (hero.damage * enemy.defence)));
-            System.out.println(hero.name + " health is: " + df.format(heroHealth));
-            System.out.println(enemy.name + " health is: " + df.format(enemyHealth));
-            System.out.println();
-        }
-        result(hero, enemy);
     }
 }
