effb1219e9b271d91f9037ac406186844b8db7c6
KarolisGib <karolisgibavicius@yahoo.com>
2024-05-12 23:25:27 +0300

    Reworked att/def arrayLists

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,31 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        Random rnd = new Random();
+        Random random = new Random();
 
         Hero hero = new Hero();
         hero.name = "Hero";
         hero.health = 100;
-        hero.damage = (rnd.nextInt(2) + 1);
-        hero.defence = (rnd.nextInt(51));
+        hero.damage = (random.nextInt(2) + 1);
+        hero.defence = (random.nextInt(51));
 
         Enemy enemy = new Enemy();
         enemy.name = "Enemy";
         enemy.health = 100;
-        enemy.damage = (rnd.nextInt(2) + 1);
-        enemy.defence = (rnd.nextInt(51));
+        enemy.damage = (random.nextInt(2) + 1);
+        enemy.defence = (random.nextInt(51));
 
         Sword rustySword = new Sword();
         rustySword.name = "Rusty sword";
-        rustySword.damage = (rnd.nextInt(2) + 8); // 8-9
+        rustySword.damage = (random.nextInt(2) + 8); // 8-9
 
         Sword sword = new Sword();
         sword.name = "Sword";
-        sword.damage = (rnd.nextInt(2) + 11); // 11-12
+        sword.damage = (random.nextInt(2) + 11); // 11-12
 
         Sword enhancedSword = new Sword();
         enhancedSword.name = "Enhanced sword";
-        enhancedSword.damage = (rnd.nextInt(2) + 14); // 14-15
+        enhancedSword.damage = (random.nextInt(2) + 14); // 14-15
 
         Shield rustyShield = new Shield();
         rustyShield.name = "Rusty shield";
@@ -42,27 +42,28 @@ public class Main {
         enhancedShield.name = "Enhanced shield";
         enhancedShield.defence = 50;
 
-        ArrayList<Integer> heroAttackChoice = new ArrayList<>();
-        heroAttackChoice.add(rustySword.damage + hero.damage);
-        heroAttackChoice.add(sword.damage + hero.damage);
-        heroAttackChoice.add(enhancedSword.damage + hero.damage);
+        ArrayList<Sword> heroAttackChoice = new ArrayList<>();
+        heroAttackChoice.add(rustySword);
+        heroAttackChoice.add(sword);
+        heroAttackChoice.add(enhancedSword);
 
-        ArrayList<Integer> enemyAttackChoice = new ArrayList<>();
-        enemyAttackChoice.add(rustySword.damage + enemy.damage);
-        enemyAttackChoice.add(sword.damage + enemy.damage);
-        enemyAttackChoice.add(enhancedSword.damage + enemy.damage);
+        ArrayList<Sword> enemyAttackChoice = new ArrayList<>();
+        enemyAttackChoice.add(rustySword);
+        enemyAttackChoice.add(sword);
+        enemyAttackChoice.add(enhancedSword);
 
-        ArrayList<Double> heroDefenceChoice = new ArrayList<>();
-        heroDefenceChoice.add(((rustyShield.defence + hero.defence) / 2) / 100);
-        heroDefenceChoice.add(((shield.defence + hero.defence) / 2) / 100);
-        heroDefenceChoice.add(((enhancedShield.defence + hero.defence) / 2) / 100);
+        ArrayList<Shield> heroDefenceChoice = new ArrayList<>();
+        heroDefenceChoice.add(rustyShield);
+        heroDefenceChoice.add(shield);
+        heroDefenceChoice.add(enhancedShield);
 
-        ArrayList<Double> enemyDefenceChoice = new ArrayList<>();
-        enemyDefenceChoice.add(((rustyShield.defence + enemy.defence) / 2) / 100);
-        enemyDefenceChoice.add(((shield.defence + enemy.defence) / 2) / 100);
-        enemyDefenceChoice.add(((enhancedShield.defence + enemy.defence) / 2) / 100);
+        ArrayList<Shield> enemyDefenceChoice = new ArrayList<>();
+        enemyDefenceChoice.add(rustyShield);
+        enemyDefenceChoice.add(shield);
+        enemyDefenceChoice.add(enhancedShield);
 
-        Methods.fight(hero, enemy, heroAttackChoice, enemyAttackChoice,
+        Methods.fight(hero, enemy,
+                heroAttackChoice, enemyAttackChoice,
                 heroDefenceChoice, enemyDefenceChoice);
     }
 }
--- src/Methods.java
+++ src/Methods.java
@@ -7,6 +7,10 @@ public class Methods {
     private static final DecimalFormat df = new DecimalFormat("0.0");
     static int counter = 0;
     static Random random = new Random();
+    static double heroHit;
+    static double enemyHit;
+    static double heroHealth;
+    static double enemyHealth;
 
     static void info(Hero hero, Enemy enemy) {
         System.out.println(hero.name + " health is: " + hero.health);
@@ -14,29 +18,43 @@ public class Methods {
         System.out.println();
     }
 
+    static void choices(Hero hero, Enemy enemy,
+                        ArrayList<Sword> heroAttackChoice, ArrayList<Sword> enemyAttackChoice,
+                        ArrayList<Shield> heroDefenceChoice, ArrayList<Shield> enemyDefenceChoice) {
+        int heroDamage = heroAttackChoice.get(random.nextInt(heroAttackChoice.size())).damage
+                + hero.damage;
+        int enemyDamage = enemyAttackChoice.get(random.nextInt(enemyAttackChoice.size())).damage
+                + enemy.damage;
+        double heroDefence = ((heroDefenceChoice.get(random.nextInt(heroDefenceChoice.size())).defence
+                + hero.defence) / 100) / 2;
+        double enemyDefence = ((enemyDefenceChoice.get(random.nextInt(enemyDefenceChoice.size())).defence
+                + enemy.defence) / 100) / 2;
+        heroHit = (heroDamage - (heroDamage * enemyDefence));
+        enemyHit = (enemyDamage - (enemyDamage * heroDefence));
+    }
+
     static void fight(Hero hero, Enemy enemy,
-                      ArrayList<Integer> heroAttackChoice, ArrayList<Integer> enemyAttackChoice,
-                      ArrayList<Double> heroDefenceChoice, ArrayList<Double> enemyDefenceChoice) {
+                      ArrayList<Sword> heroAttackChoice, ArrayList<Sword> enemyAttackChoice,
+                      ArrayList<Shield> heroDefenceChoice, ArrayList<Shield> enemyDefenceChoice) {
         info(hero, enemy);
-        int heroDamage = heroAttackChoice.get(random.nextInt(heroAttackChoice.size()));
-        int enemyDamage = enemyAttackChoice.get(random.nextInt(enemyAttackChoice.size()));
-        double heroDefence = heroDefenceChoice.get(random.nextInt(heroDefenceChoice.size()));
-        double enemyDefence = enemyDefenceChoice.get(random.nextInt(enemyDefenceChoice.size()));
+        choices(hero, enemy, heroAttackChoice, enemyAttackChoice, heroDefenceChoice, enemyDefenceChoice);
         while (hero.health > 0 && enemy.health > 0) {
             counter++;
-            double heroHealth = (hero.health -= (enemyDamage - (enemyDamage * heroDefence)));
-            double enemyHealth = (enemy.health -= (heroDamage - (heroDamage * enemyDefence)));
-            System.out.println(hero.name + " health is: " + df.format(heroHealth));
-            System.out.println(enemy.name + " health is: " + df.format(enemyHealth));
+            heroHealth = (hero.health -= enemyHit);
+            enemyHealth = (enemy.health -= heroHit);
+            System.out.println(hero.name + " hit " + enemy.name + " with " + df.format(heroHit) +
+                    " damage and left him with " + df.format(enemyHealth) + " health");
+            System.out.println(enemy.name + " hit " + hero.name + " with " + df.format(enemyHit) +
+                    " damage and left him with " + df.format(heroHealth) + " health");
             System.out.println();
         }
         result(hero, enemy);
     }
 
     static void result(Hero hero, Enemy enemy) {
-        if (hero.health == enemy.health) {
+        if (heroHealth == enemyHealth) {
             System.out.println("Battle took " + counter + " rounds and ended up in tie");
-        } else if (hero.health > enemy.health) {
+        } else if (heroHealth > enemyHealth) {
             System.out.println("Battle took " + counter + " rounds and " + hero.name + " won");
         } else {
             System.out.println("Battle took " + counter + " rounds and " + enemy.name + " won");
